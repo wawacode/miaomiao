9e4b034ee82071d019ce631c98401a669517b49a
allen2 <tianyixingkai@126.com>
2015-01-08 17:31:43 +0800

    test

--- dev/jy/src/main/java/com/renren/ntc/sg/biz/dao/DeviceDAO.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/biz/dao/DeviceDAO.java
@@ -2,6 +2,7 @@ package com.renren.ntc.sg.biz.dao;
 
 import com.renren.ntc.sg.bean.Device;
 import net.paoding.rose.jade.annotation.DAO;
+import net.paoding.rose.jade.annotation.ReturnGeneratedKeys;
 import net.paoding.rose.jade.annotation.SQL;
 
 import java.util.List;
@@ -16,7 +17,7 @@ import java.util.List;
 @DAO(catalog = "ABC")
 public interface DeviceDAO {
     static final String TABLE_NAME= "device";
-
+    static final String  FIELDS = "shop_id,type ,status ,token , secret_key"  ;
     @SQL("select * from " + TABLE_NAME +" where id =:1")
     public Device getDev(long pid) ;
 
@@ -26,5 +27,7 @@ public interface DeviceDAO {
     @SQL("update " + TABLE_NAME +" set status = :2 , update_time = now() where id =:1 ")
     public int update(long pid, String status) ;
 
-
+    @ReturnGeneratedKeys
+    @SQL("insert into " + TABLE_NAME + "(" + FIELDS +" ) values"  + " (:1.shop_id,:1.type,:1.status,:1.token,:1.secret_key)")
+    public long insert(Device o);
 }
--- dev/jy/src/main/java/com/renren/ntc/sg/biz/dao/ShopDAO.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/biz/dao/ShopDAO.java
@@ -2,6 +2,7 @@ package com.renren.ntc.sg.biz.dao;
 
 import com.renren.ntc.sg.bean.Shop;
 import net.paoding.rose.jade.annotation.DAO;
+import net.paoding.rose.jade.annotation.ReturnGeneratedKeys;
 import net.paoding.rose.jade.annotation.SQL;
 
 import java.util.List;
@@ -21,9 +22,9 @@ public interface ShopDAO {
 
     @SQL("select " +FIELDS  + "  from "  + TABLE_NAME + " where  owner_user_id = :1 ")
     public Shop getShopbyOwner_id(long id);
-	
-	
-	@SQL("insert into " + TABLE_NAME + "(" + FIELDS +" ) values"  + " (:1.name,:1.owner_phone,:1.head_url,:1.shop_url,:1.lng,:1,lat)")
+
+    @ReturnGeneratedKeys
+	@SQL("insert into " + TABLE_NAME + "(" + INSERT_FIELDS +" ) values"  + " (:1.owner_user_id,:1.name,:1.owner_phone,:1.head_url,:1.shop_url,:1.lng,:1.lat)")
 	public int insert(Shop o);
 	
 	
