b6ef1788d6b782d0d47049d067ba4a8e8a547f74
allen2 <tianyixingkai@126.com>
2015-01-09 21:32:17 +0800

    add

--- dev/jy/src/main/java/com/renren/ntc/sg/controllers/catstaff/HomeController.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/controllers/catstaff/HomeController.java
@@ -38,41 +38,41 @@ public class HomeController {
      */
     @Get("commit")
     @Post("commit")
-    public String index( Invocation inv, @Param("staff_phone") String staff_phone,
-                         @Param("staff_name") String staff_name,@Param("staff_pwd") String staff_pwd,
-                         @Param("shop_name") String shop_name,
-                         @Param("shop_tel") String shop_tel,
-                         @Param("shop_print") String shop_print,
-                         @Param("shop_lat") double shop_lat,
-                         @Param("shop_lng") double shop_lng) {
-            if (!legal(staff_phone,staff_name,staff_pwd)){
-                return "@" + Constants.PARATERERROR;
-            }
-             if (StringUtils.isBlank(shop_name)||StringUtils.isBlank(shop_tel)||
-                     StringUtils.isBlank(shop_print)
-                     ||StringUtils.isBlank(shop_print)||
-                     StringUtils.isBlank(shop_name)||
-                     0  == shop_lat||
-                     0 == shop_lng) {
+    public String index(Invocation inv, @Param("staff_phone") String staff_phone,
+                        @Param("staff_name") String staff_name, @Param("staff_pwd") String staff_pwd,
+                        @Param("shop_name") String shop_name,
+                        @Param("shop_tel") String shop_tel,
+                        @Param("shop_print") String shop_print,
+                        @Param("shop_lat") double shop_lat,
+                        @Param("shop_lng") double shop_lng) {
+        if (!legal(staff_phone, staff_name, staff_pwd)) {
+            return "@" + Constants.PARATERERROR;
+        }
+        if (StringUtils.isBlank(shop_name) || StringUtils.isBlank(shop_tel) ||
+                StringUtils.isBlank(shop_print)
+                || StringUtils.isBlank(shop_print) ||
+                StringUtils.isBlank(shop_name) ||
+                0 == shop_lat ||
+                0 == shop_lng) {
 
-                 return "@" + Constants.PARATERERROR;
+            return "@" + Constants.PARATERERROR;
 
-             }
-                CatStaffCommit catStaffCommit  = new CatStaffCommit();
-                catStaffCommit.setName(staff_name);
-                catStaffCommit.setPhone(staff_phone);
-                catStaffCommit.setPwd(staff_pwd);
-                catStaffCommit.setShop_name(shop_name);
-                catStaffCommit.setShop_tel(shop_tel);
-                catStaffCommit.setShop_print(shop_print);
-                catStaffCommit.setShop_lat(shop_lat);
-                catStaffCommit.setShop_lng(shop_lng);
-                long catstaff_id =  catStaffCommitDAO.insert(catStaffCommit);
-                JSONObject re = createShopService.createShop(catstaff_id);
-                JSONObject jb =  new JSONObject();
-                jb.put("code",0);
-                jb.put("url","http://www.mbianli.com/sg/shop?shop_id=" + re.getLong("shop_id"));
-                return "@" + jb.toJSONString();
+        }
+        CatStaffCommit catStaffCommit = new CatStaffCommit();
+        catStaffCommit.setName(staff_name);
+        catStaffCommit.setPhone(staff_phone);
+        catStaffCommit.setPwd(staff_pwd);
+        catStaffCommit.setShop_name(shop_name);
+        catStaffCommit.setShop_tel(shop_tel);
+        catStaffCommit.setShop_print(shop_print);
+        catStaffCommit.setShop_lat(shop_lat);
+        catStaffCommit.setShop_lng(shop_lng);
+        long catstaff_id = catStaffCommitDAO.insert(catStaffCommit);
+        JSONObject re = createShopService.createShop(catstaff_id);
+        JSONObject jb = new JSONObject();
+        jb.put("code", 0);
+        jb.put("url", "http://www.mbianli.com/sg/shop?shop_id=" + re.getLong("shop_id"));
+        return "@" + jb.toJSONString();
     }
 
     private boolean legal(String phone, String staff_name, String staff_pwd) {
@@ -84,7 +84,7 @@ public class HomeController {
 
     @Get("test")
     @Post("test")
-    public String  test (Invocation inv){
+    public String test(Invocation inv) {
         return "tool";
     }
 
