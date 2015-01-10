514ad916680857f8bdd9c6d602f0f343005385f2
allen2 <tianyixingkai@126.com>
2015-01-10 15:51:06 +0800

    message

--- dev/jy/src/main/java/com/renren/ntc/sg/util/Test.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/util/Test.java
@@ -1,5 +1,11 @@
 package com.renren.ntc.sg.util;
 
+import com.alibaba.fastjson.JSONObject;
+import com.renren.ntc.sg.bean.CatStaffCommit;
+import com.renren.ntc.sg.biz.dao.*;
+import com.renren.ntc.sg.service.CreateShopService;
+import net.paoding.rose.scanning.context.RoseAppContext;
+
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 
@@ -14,7 +20,25 @@ public class Test {
 
     public static void main(String [] args){
 
+        RoseAppContext rose = new RoseAppContext();
+        ShopDAO shopDao  = rose.getBean(ShopDAO.class);
+        ItemsDAO itemDao  = rose.getBean(ItemsDAO.class);
+        DeviceDAO deviceDao  = rose.getBean(DeviceDAO.class);
+        ShopCategoryDAO shopCategoryDao  = rose.getBean(ShopCategoryDAO.class);
+        CatStaffCommitDAO catStaffCommitDAO  = rose.getBean(CatStaffCommitDAO.class);
+        CreateShopService createShopService = rose.getBean(CreateShopService.class) ;
 
-
+        CatStaffCommit catStaffCommit = new CatStaffCommit();
+        catStaffCommit.setName("");
+        catStaffCommit.setPhone("");
+        catStaffCommit.setPwd("staff_pwd");
+        catStaffCommit.setShop_name("shop_name");
+        catStaffCommit.setShop_tel("shop_tel");
+        catStaffCommit.setShop_print("shop_print");
+        catStaffCommit.setShop_lat(3.222);
+        catStaffCommit.setShop_lng(3.222);
+        long catstaff_id = catStaffCommitDAO.insert(catStaffCommit);
+        JSONObject re = createShopService.createShop(catstaff_id);
+        System.out.println(re.toJSONString());
     }
 }
