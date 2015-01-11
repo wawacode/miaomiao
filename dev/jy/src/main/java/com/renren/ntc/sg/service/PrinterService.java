d96f80a4c46ef719c84f1af045cfb4c907b7c900
allen2 <tianyixingkai@126.com>
2015-01-11 21:53:21 +0800

    mm

--- dev/jy/src/main/java/com/renren/ntc/sg/service/PrinterService.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/service/PrinterService.java
@@ -202,7 +202,7 @@ public class PrinterService {
         String name = od.getString("name");
         int count = od.getIntValue("count");
         int price = od.getIntValue("price");
-        float pp = price /100 ;
+        float pp = (float)price /100 ;
         sb.append(name);
         int a = 22 - length(name);
         if (a <= 0) {
