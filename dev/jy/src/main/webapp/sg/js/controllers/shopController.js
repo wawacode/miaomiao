d0ca97eca18dc9ca266d1aaaf5c0debd9e516d7b
jiliang <liangji101@yahoo.cn>
2015-01-19 11:31:51 +0800

    update js

--- dev/jy/src/main/webapp/sg/js/controllers/shopController.js
+++ dev/jy/src/main/webapp/sg/js/controllers/shopController.js
@@ -44,9 +44,49 @@ angular.module('miaomiao.shop', ['ionic', 'LocalStorageModule'])
 
         $urlRouterProvider.otherwise('/productlist');
 
-    }).controller('ProductCtrl', function ($scope, $ionicLoading, $http, $state, localStorageService) {
-//        $scope.shop = {};
-//        $scope.shop.name = "test";
+    }).controller('ProductCtrl', function ($scope, $ionicLoading, $ionicPopup, $http, $state, localStorageService, httpClient) {
+        $scope.shop = $scope.shop || {};
+
+        httpClient.getProductList($scope.shop.id, function (data, status) {
+
+            var code = data.code, dataDetail = data.data;
+            if (!code == 0) {
+                $ionicPopup.alert({
+                    title: '加载数据失败',
+                    template: ''
+                });
+                return;
+            }
+
+            $scope.shop = dataDetail.shop;
+            $scope.categoryls = dataDetail.categoryls;
+            // extend for use
+            /*
+             {"category_id":15,"category_sub_id":0,"id":0,
+             "itemls":[{"category_id":15,"category_sub_id":0,"count":956,"create_time":1419821656000,"ext":0,"id":28062,"name":"哈哈镜鸭爪买一赠一",
+             "pic_url":"http://www.mbianli.com/cat/images/lelin/HHJ001.jpg","price":1600,"price_new":0,
+             "score":99999,"serialNo":"HHJ001","shop_id":1,"status":0}
+             * */
+           for(var idx=0;idx< $scope.categoryls.length;idx++){
+               $scope.categoryls.totolCnt = 0;
+               $scope.categoryls.selected = 0;
+               if(idx == 0){
+                   $scope.categoryls.selected = 1;
+               }
+               for(var item_idx =0;item_idx < $scope.categoryls.items.length;item_idx++){
+                   $scope.categoryls.items.selectedCnt = 0;
+               }
+           }
+
+           $scope.currentDisplayItems = $scope.categoryls.length && $scope.categoryls[0].items;
+
+        }, function (data, status) {
+            $ionicPopup.alert({
+                title: '加载数据失败',
+                template: ''
+            });
+            return;
+        });
     })
     .controller('CheckoutCtrl', function ($scope, $ionicLoading, $http, $state, localStorageService) {
 
@@ -62,5 +102,13 @@ angular.module('miaomiao.shop', ['ionic', 'LocalStorageModule'])
     })
     .controller('MyOrdersCtrl', function ($scope, $ionicLoading, $http, $state, localStorageService) {
 
-    });
+    }).directive('backImg', function(){
+    return function(scope, element, attrs){
+        var url = attrs.backImg;
+        element.css({
+            'background-image': 'url(' + url +')',
+            'background-size' : 'cover'
+        });
+    };
+});
 
--- dev/jy/src/main/webapp/sg/js/services/httpClient.js
+++ dev/jy/src/main/webapp/sg/js/services/httpClient.js
@@ -1,15 +1,21 @@
 angular.module('miaomiao.shop', ['ionic'])
     .factory('httpClient', ['$http', function ($http) {
 
-        var doRequest = function (path) {
-            return $http({
+        var doRequest = function (path,params,success,fail) {
+            $http({
                 method: 'GET',
-                url: path
-            });
+                url: path + '?' + params
+            }).
+                success(function (data, status, headers, config) {
+                    success(data, status, headers, config)
+                }).
+                error(function (data, status, headers, config) {
+                    fail (data, status, headers, config)
+                });;
         }
         return {
-            getProductList: function () {
-                return doRequest('productList');
+            getProductList: function (shopId,success,fail) {
+                return doRequest('category/get','shop_id='+shopId,success,fail);
             }
         };
     }]);
