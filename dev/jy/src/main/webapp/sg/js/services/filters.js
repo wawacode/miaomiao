da22f5ea4088ad2c6736f1777c2b1403b54813dc
jiliang <liangji101@yahoo.cn>
2015-01-23 18:03:10 +0800

    update shop address

--- dev/jy/src/main/webapp/sg/js/services/filters.js
+++ dev/jy/src/main/webapp/sg/js/services/filters.js
@@ -1,22 +1,27 @@
-angular.module('miaomiao.shop').filter('getTotolCount', function() {
-        return function(input) {
-            input = input || [];
+angular.module('miaomiao.shop').filter('getTotolCount',function () {
+    return function (input) {
+        input = input || [];
 
-            var total = 0;
-            for (var item_idx = 0; item_idx < input.length; item_idx++) {
-                total += parseInt(input[item_idx].count || 0);
-            }
-            return total;
-        };
-    }).filter('getTotolPrice', function() {
-        return function(input) {
+        var total = 0;
+        for (var item_idx = 0; item_idx < input.length; item_idx++) {
+            total += parseInt(input[item_idx].count || 0);
+        }
+        return total;
+    };
+}).filter('getTotolPrice',function () {
+        return function (input) {
             input = input || [];
 
             var total = 0.0;
             for (var item_idx = 0; item_idx < input.length; item_idx++) {
                 total += parseFloat(input[item_idx].price || 0.0) * parseInt(input[item_idx].count || 0);
             }
-            return total/100.0;
+            return total / 100.0;
+        };
+    }).filter('removeAMPM', function () {
+        return function (text) {
+            return text.replace(/AM/, '').replace(/PM/, ''); // Characters after Colon
         };
     });
+;
 
