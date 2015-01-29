63a4112bfe61a6edfd2ab8bb3f19f071e50fc745
leehom316 <chunhai.li@lichunhai-PC.qunarservers.com>
2015-01-29 16:57:49 +0800

    basicController

---
+++ dev/jy/src/main/java/com/renren/ntc/sg/controllers/console/api/BasicConsoleController.java
@@ -0,0 +1,48 @@
+package com.renren.ntc.sg.controllers.console.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.fastjson.JSONObject;
+import com.renren.ntc.sg.bean.Shop;
+import com.renren.ntc.sg.biz.dao.ShopDAO;
+import com.renren.ntc.sg.service.LoggerUtils;
+
+public class BasicConsoleController {
+	
+	@Autowired
+	private ShopDAO shopDAO;
+	
+	protected String getDataResult(int code,JSONObject data) {
+		JSONObject result = new JSONObject();
+		result.put("code", code);
+		result.put("data", data);
+		return result.toJSONString();
+	}
+	
+	protected String getActionResult(int code,String message) {
+		JSONObject result = new JSONObject();
+		result.put("code", code);
+		result.put("msg", message);
+		return result.toJSONString();
+	}
+	
+	protected JSONObject initJSONResult(int code,String message) {
+		JSONObject result = new JSONObject();
+		result.put("code", code);
+		result.put("msg", message);
+		return result;
+	}
+	
+	protected Shop isExistShop(long shop_id) {
+		  if (0  >= shop_id){
+			  return null;
+	        }
+	        Shop shop = shopDAO.getShop(shop_id);
+
+	        if(null == shop){
+	            LoggerUtils.getInstance().log(String.format("can't find shop  %d  " ,shop_id) );
+	            return null;
+	        }
+	        return shop;
+	}
+}
