ce952c8fb6671ca238a82edd81604533e6f4944d
allen2 <tianyixingkai@126.com>
2015-01-31 14:13:41 +0800

    mm

--- dev/crontab/src/main/java/com/renren/ntc/sg/biz/dao/OrdersDAO.java
+++ dev/crontab/src/main/java/com/renren/ntc/sg/biz/dao/OrdersDAO.java
@@ -2,10 +2,12 @@ package com.renren.ntc.sg.biz.dao;
 
 import com.renren.ntc.sg.bean.Order;
 import com.renren.ntc.sg.bean.OrderInfo;
+import com.renren.ntc.sg.bean.TopUser;
 import net.paoding.rose.jade.annotation.DAO;
 import net.paoding.rose.jade.annotation.SQL;
 import net.paoding.rose.jade.annotation.SQLParam;
 
+import java.util.Date;
 import java.util.List;
 
 /*
@@ -27,41 +29,44 @@ CREATE TABLE `items` (
 
 @DAO(catalog = "ABC")
 public interface OrdersDAO {
-    static final String TABLE_NAME= "orders";
     static final String FIELDS = "id, order_id,shop_id,user_id,address_id,remarks ,info ,snapshot,status,price,create_time,update_time" ;
     static final String INSERT_FIELDS = " order_id,shop_id,user_id,address_id,remarks ,info,snapshot,status,price" ;
 
+	@SQL("select "+ FIELDS +" from ##(:tableName)   where user_id =:1 order by create_time desc limit :2,:3")
+	public List<Order> getOrder(long user_id, int start, int offset ,@SQLParam("tableName") String tableName);
+
+
+    @SQL("select "+ FIELDS +" from ##(:tableName)   where shop_id =:1 order by create_time desc limit :2,:3")
+    public List<Order> getOrderByShop(long shop_id, int start, int offset,@SQLParam("tableName") String tableName);
+
+
+    @SQL("insert into  ##(:tableName) (" + INSERT_FIELDS + ") values(:1.order_id,:1.shop_id," +
+            ":1.user_id,:1.address_id,:1.remarks,:1.info,:1.snapshot,:1.status,:1.price)  ")
+    public int  insertUpdate(Order o,@SQLParam("tableName") String tableName);
+
+    @SQL("select "+ FIELDS +" from ##(:tableName)  where shop_id =:1 and status = 1 ")
+    public List<Order> getOrder2Print(long shop_id,@SQLParam("tableName") String tableName);
+
+    @SQL("select "+ FIELDS +" from ##(:tableName)  where shop_id =:1 order by create_time desc limit :2,:3 ")
+    List<Order> get10Orders(long shop_id,int from, int offset,@SQLParam("tableName") String tableName);
+
+    @SQL("select "+ FIELDS +" from ##(:tableName)  order by create_time desc limit 0,100 ")
+    List<Order> get10Orders(@SQLParam("tableName") String tableName);
+
+    @SQL("update ##(:tableName)   set status=:1 where order_id = :2 ")
+    int update(int i, String orderId,@SQLParam("tableName") String tableName);
 
     @SQL("select "+ FIELDS +" from ##(:tableName)  where order_id =:1 ")
     public Order getOrder(String orderId,@SQLParam("tableName") String tableName);
-
+    
     @SQL("select "+ FIELDS +" from ##(:tableName)  where create_time between :1 and :2")
     public List<Order> getOrder(String beginTime,String endTime,@SQLParam("tableName") String tableName);
 
-    @SQL("select "+ FIELDS +" from ##(:tableName)   where user_id =:1 order by create_time desc limit :2,:3")
-    public List<Order> getOrder(long user_id, int start, int offset ,@SQLParam("tableName") String tableName);
-
-    @SQL("select "+ FIELDS +" from " + TABLE_NAME + "  where shop_id =:1 order by create_time desc limit :2,:3")
-    public List<Order> getOrderByShop(long shop_id, int start, int offset);
+    @SQL("select count(*) as count ,address_id from ##(:tableName)  where create_time>:2 and create_time<:3 group by address_id order by count desc ")
+    public List<TopUser> getTop100User(@SQLParam("tableName") String tableName,Date start, Date end);
 
 
-    @SQL("insert into  " + TABLE_NAME + "(" + INSERT_FIELDS + ") values(:1.order_id,:1.shop_id," +
-            ":1.user_id,:1.address_id,:1.remarks,:1.info,:1.snapshot,:1.status,:1.price)  ")
-    public int  insertUpdate(Order o);
-
-    @SQL("select "+ FIELDS +" from " + TABLE_NAME + "  where shop_id =:1 and status = 1 ")
-    public List<Order> getOrder2Print(long shop_id);
-
-    @SQL("select "+ FIELDS +" from " + TABLE_NAME + " where shop_id = :1 order by create_time desc limit :2,:3 ")
-    List<Order> get10Orders(long shop_id,int from ,int offset);
-
-    @SQL("select "+ FIELDS +" from " + TABLE_NAME + " order by create_time desc limit 0,100 ")
-    List<Order> get10Orders();
-
-    @SQL("update " + TABLE_NAME + "   set status=:1 where order_id = :2 ")
-    int update(int i, String orderId);
-
-    @SQL("select "+ FIELDS +" from " + TABLE_NAME + "  where order_id =:1 ")
-    public Order getOrder(String orderId);
+    @SQL("select "+ FIELDS +" from ##(:tableName)   where address_id =:2 order by create_time desc")
+    List<Order> getOrderByAddressId(@SQLParam("tableName") String tableName,long id);
 
 }
