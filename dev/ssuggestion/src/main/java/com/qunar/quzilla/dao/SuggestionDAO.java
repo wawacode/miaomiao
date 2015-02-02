f1f640e8e5ad7504577c3a28cf12f1a61172663f
allen2 <tianyixingkai@126.com>
2015-02-02 14:36:41 +0800

    mm

--- dev/ssuggestion/src/main/java/com/qunar/quzilla/dao/SuggestionDAO.java
+++ dev/ssuggestion/src/main/java/com/qunar/quzilla/dao/SuggestionDAO.java
@@ -16,18 +16,18 @@ import com.qunar.quzilla.pojo.SDoc;
  */
 @DAO
 public interface SuggestionDAO {
-    static final  String   FIELDS = "id,`word`";
+    static final  String   FIELDS = "id,`word` ";
     @SQL("select " + FIELDS + " from s_suggestion where shop_id=:1 and audit = 0  order by `score` desc ")
     List<SDoc> getDoc(long shop_id);
     
     @SQL("select " +FIELDS + " from s_suggestion where shop_id=:3 and audit = 0  order by `score` desc limit :1,:2 ")
     List<SDoc> getDoc(int offset , int count,long shop_id);
 
-    @SQL("select count(id) as count from s_suggestion  where audit = 0 ")
-	Count getCount();
+    @SQL("select count(id) as count from s_suggestion  where audit = 0 and shop_id=:1")
+	Count getCount(long shop_id);
     
     @ReturnGeneratedKeys
-    @SQL("insert    into s_suggestion  (word,score) values(:1,:2)")
-    int add(String key, int o);
+    @SQL("insert    into s_suggestion  (word,score,shop_id) values(:1,:2,:3)")
+    int add(String key, int o,long shop_id);
 
 }
