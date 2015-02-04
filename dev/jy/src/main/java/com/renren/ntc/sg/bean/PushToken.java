bcf9ab2ea9965ac3725215a73d1abaf448f36d71
allen2 <tianyixingkai@126.com>
2015-02-04 17:44:57 +0800

    mmm

---
+++ dev/jy/src/main/java/com/renren/ntc/sg/bean/PushToken.java
@@ -0,0 +1,57 @@
+package com.renren.ntc.sg.bean;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yunming.zhu
+ * Date: 14-12-8
+ * Time: 下午12:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class PushToken {
+
+
+    private long   id  ;
+    private String ower_phone ;
+
+
+    private String device_token ;
+    private Date create_time;
+
+
+    public String getDevice_token() {
+        return device_token;
+    }
+
+    public void setDevice_token(String device_token) {
+        this.device_token = device_token;
+    }
+
+    public String getOwer_phone() {
+        return ower_phone;
+    }
+
+    public void setOwer_phone(String ower_phone) {
+        this.ower_phone = ower_phone;
+    }
+
+
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
