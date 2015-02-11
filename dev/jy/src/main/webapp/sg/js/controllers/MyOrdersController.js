f142e9326eea9b23fb1eaeb423d1786449601342
jiliang <liangji101@yahoo.cn>
2015-02-11 22:42:21 +0800

    fix my order page

--- dev/jy/src/main/webapp/sg/js/controllers/MyOrdersController.js
+++ dev/jy/src/main/webapp/sg/js/controllers/MyOrdersController.js
@@ -1,5 +1,7 @@
 angular.module('miaomiao.shop')
-    .controller('MyOrdersCtrl',function ($scope, $ionicLoading, $http, $state, localStorageService,$sessionStorage,httpClient,AddressService,OrderService) {
+    .controller('MyOrdersCtrl',function ($scope, $ionicLoading,$ionicPopup, $http, $state,$ionicScrollDelegate,localStorageService,$sessionStorage,httpClient,AddressService,OrderService,MMUtils) {
+
+        $scope.shop = localStorageService.get('MMMETA_shop') || {};
 
         $scope.goToAddressList = function(){
             $state.go('userAddressList', null, { reload: true });
@@ -20,8 +22,58 @@ angular.module('miaomiao.shop')
                 }
             }
         }
+        $scope.addr = {};
+        $scope.addNewAddressInOrderPage = function(){
 
-        $scope.shop = localStorageService.get('MMMETA_shop');
+            if(!$scope.addr.address || !$scope.addr.phone || !MMUtils.isValidTelNumber($scope.addr.phone)){
+
+                $ionicPopup.alert({
+                    title: '请填写正确的地址电话',
+                    template: ''
+                });
+
+                return;
+            }
+
+            $scope.LoadingMessage = '正在添加新地址...';
+            $ionicLoading.show({
+                templateUrl: '/views/sg/templates/loadingIndicator.html',
+                scope: $scope
+            });
+
+            var shopId = $scope.shop && $scope.shop.id || 1 ; // make a default one
+            httpClient.addAddress(shopId, $scope.addr, function(data, status){
+
+                var code = data.code, dataDetail = data.data;
+                if (code != 0) {
+                    $ionicPopup.alert({
+                        title: '添加新地址失败:' + data.msg,
+                        template: ''
+                    });
+                    return;
+                }
+
+                $ionicLoading.hide();
+
+                AddressService.addressChangeEventAddNew($scope.addr);
+
+                $ionicScrollDelegate.resize();
+
+                $state.go('myOrders',null, { reload: true });
+
+
+            },function(data, status){
+
+                $ionicLoading.hide();
+
+                $ionicPopup.alert({
+                    title: '添加新地址失败,重试',
+                    template: ''
+                });
+
+                $ionicScrollDelegate.resize();
+            })
+        }
 
         $scope.info = {};
         $scope.info.hasOrder = true;
@@ -66,15 +118,28 @@ angular.module('miaomiao.shop')
                     $sessionStorage.orderAddresses = $scope.addressls;
                     $sessionStorage.orderOrders = $scope.orders;
 
+                    $ionicScrollDelegate.resize();
+
                 },function(data, status){
 
                     $scope.info.hasOrder = false;
                     $scope.info.hasAddress = false;
+                    $ionicScrollDelegate.resize();
 
                     $ionicLoading.hide();
                 });
             }
 
+        }
+
+        $scope.goToShopOrFindShop = function(){
+
+            var lastShop = localStorageService.get('MMMETA_shop');
+            if (lastShop && lastShop.id) {
+                $state.go('productList',null,{reload:true});
+            } else {
+                $state.go('findshop',null,{reload:true});
+            }
         }
 
         OrderService.orderChangeEventSuccess();
@@ -88,6 +153,7 @@ angular.module('miaomiao.shop')
         });
 
         $scope.$on("$ionicView.enter", function () {
+            $scope.shop = localStorageService.get('MMMETA_shop') || {};
             reloadInfo();
         });
     });
