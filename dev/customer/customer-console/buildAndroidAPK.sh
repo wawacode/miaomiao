05b6a8beb44828b0efacac4bee49588e2c5a007e
jiliang <liangji101@yahoo.cn>
2015-02-23 11:29:42 +0800

    update apk

---
+++ dev/customer/customer-console/buildAndroidAPK.sh
@@ -0,0 +1,9 @@
+#
+# make sure : 1. install cordova 2. install android sdk 3. replace android skd path
+#
+
+rm -fr mmconsole.apk
+cordova build --release android
+echo 0000001 | jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore  platforms/android/ant-build/CordovaApp-release-unsigned.apk alias_name
+ ~/android-sdks/build-tools/21.1.2/zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk mmconsole.apk
+
