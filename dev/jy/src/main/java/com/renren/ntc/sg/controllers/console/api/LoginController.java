e3f199fff1029bc26911a53a43abe43bf899fd15
allen2 <tianyixingkai@126.com>
2015-03-03 17:58:36 +0800

    mm

--- dev/jy/src/main/java/com/renren/ntc/sg/controllers/console/api/LoginController.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/controllers/console/api/LoginController.java
@@ -69,10 +69,6 @@ public class LoginController extends BasicConsoleController {
         result.put("code", -1);
         result.put("msg", "用户不存在");
         result.put("origURL", origURL);
-//        RegistUser user = hostHolder.getUser();
-//		if (user != null) {
-//			 return "@json:" + result.toJSONString();
-//		}
         if (origURL == null || origURL.equals("")) {
             origURL = Constants.DOMAIN;
         }
@@ -114,8 +110,19 @@ public class LoginController extends BasicConsoleController {
         if (null == u) {
             Catstaff c = catstaffDao.getCatStaff(phone, pwd);
             if (null != c) {
+                if(ifkf(c)){
+                    List<Shop> shops = shopDAO.getAllShopsByAudit(1);
+                    CookieManager.getInstance().saveCookie(inv.getResponse(), Constants.COOKIE_KEY_REGISTUSER,
+                            SUtils.wrapper(SUtils.getStaffKey(c.getId())));
+                    JSONObject resultJson = new JSONObject();
+                    JSONArray s = (JSONArray) JSON.toJSON(shops);
+                    resultJson.put("shop", shops);
+                    resultJson.put("token", SUtils.wrapper(SUtils.getStaffKey(c.getId())));
+                    return "@json:" + getDataResult(0, resultJson);
+                }
                 List<Long> shop_ids = catstaffCommitDao.getShop_ids(c.getPhone());
                 List<Shop> shops = shopDAO.getShops(shop_ids);
+
                 if (null == shops || shops.size() == 0) {
                     result.put("code", -3);
                     result.put("msg", "没有可用店铺");
@@ -150,6 +157,10 @@ public class LoginController extends BasicConsoleController {
         resultJson.put("shop", shops);
         resultJson.put("token", token);
         return "@json:" + getDataResult(0, resultJson);
+    }
+
+    private boolean ifkf(Catstaff c) {
+        return c.getType() == 2 ;
     }
 
     @Get("islogin")
