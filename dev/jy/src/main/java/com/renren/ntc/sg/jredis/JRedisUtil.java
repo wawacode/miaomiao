c1df4d01a35fa18ca36d5d02b49e381450f5b19e
allen2 <tianyixingkai@126.com>
2015-03-06 21:12:26 +0800

    mm

---
+++ dev/jy/src/main/java/com/renren/ntc/sg/jredis/JRedisUtil.java
@@ -0,0 +1,46 @@
+package com.renren.ntc.sg.jredis;
+
+import com.mongodb.*;
+import com.renren.ntc.sg.util.Constants;
+import com.renren.ntc.sg.util.SUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import redis.clients.jedis.Jedis;
+
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+/**
+ * @author allen
+ */
+public class JRedisUtil {
+
+    private Jedis jds;
+
+    static Log logger = LogFactory.getLog(JRedisUtil.class);
+
+    private static JRedisUtil instance = new JRedisUtil();
+
+    private JRedisUtil() {
+        jds = new Jedis("123.56.102.224") ;
+    }
+
+    public static JRedisUtil getInstance() {
+        return instance;
+    }
+
+    public String  set(String key, String value){
+        return jds.set(key,value);
+    }
+
+    public String  get(String key){
+        return jds.get(key);
+    }
+
+    public static void main(String[] args) {
+        JRedisUtil.getInstance().set("aaa","bbb");
+        System.out.println(JRedisUtil.getInstance().get("aaa"));
+    }
+}
