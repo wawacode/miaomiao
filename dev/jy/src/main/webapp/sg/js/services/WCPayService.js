e1c3130d5133332eb4f1bb65c851aa2c96cb3883
jiliang <liangji101@yahoo.cn>
2015-03-17 14:18:38 +0800

    add wc pay

---
+++ dev/jy/src/main/webapp/sg/js/services/WCPayService.js
@@ -0,0 +1,318 @@
+function hex_md5(s) {
+    return rstr2hex(rstr_md5(str2rstr_utf8(s)));
+};
+
+//
+/*
+ * Convert a raw string to a hex string
+ */
+function rstr2hex(input) {
+    try {
+        hexcase
+    } catch (e) {
+        hexcase = 0;
+    }
+    var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
+    var output = "";
+    var x;
+    for (var i = 0; i < input.length; i++) {
+        x = input.charCodeAt(i);
+        output += hex_tab.charAt((x >>> 4) & 0x0F)
+            + hex_tab.charAt(x & 0x0F);
+    }
+    return output;
+}
+
+/*
+ * Convert a raw string to an array of little-endian words
+ * Characters >255 have their high-byte silently ignored.
+ */
+function rstr2binl(input) {
+    var output = Array(input.length >> 2);
+    for (var i = 0; i < output.length; i++)
+        output[i] = 0;
+    for (var i = 0; i < input.length * 8; i += 8)
+        output[i >> 5] |= (input.charCodeAt(i / 8) & 0xFF) << (i % 32);
+    return output;
+}
+
+/*
+ * Calculate the MD5 of an array of little-endian words, and a bit length.
+ */
+function binl_md5(x, len) {
+    /* append padding */
+    x[len >> 5] |= 0x80 << ((len) % 32);
+    x[(((len + 64) >>> 9) << 4) + 14] = len;
+
+    var a = 1732584193;
+    var b = -271733879;
+    var c = -1732584194;
+    var d = 271733878;
+
+    for (var i = 0; i < x.length; i += 16) {
+        var olda = a;
+        var oldb = b;
+        var oldc = c;
+        var oldd = d;
+
+        a = md5_ff(a, b, c, d, x[i + 0], 7, -680876936);
+        d = md5_ff(d, a, b, c, x[i + 1], 12, -389564586);
+        c = md5_ff(c, d, a, b, x[i + 2], 17, 606105819);
+        b = md5_ff(b, c, d, a, x[i + 3], 22, -1044525330);
+        a = md5_ff(a, b, c, d, x[i + 4], 7, -176418897);
+        d = md5_ff(d, a, b, c, x[i + 5], 12, 1200080426);
+        c = md5_ff(c, d, a, b, x[i + 6], 17, -1473231341);
+        b = md5_ff(b, c, d, a, x[i + 7], 22, -45705983);
+        a = md5_ff(a, b, c, d, x[i + 8], 7, 1770035416);
+        d = md5_ff(d, a, b, c, x[i + 9], 12, -1958414417);
+        c = md5_ff(c, d, a, b, x[i + 10], 17, -42063);
+        b = md5_ff(b, c, d, a, x[i + 11], 22, -1990404162);
+        a = md5_ff(a, b, c, d, x[i + 12], 7, 1804603682);
+        d = md5_ff(d, a, b, c, x[i + 13], 12, -40341101);
+        c = md5_ff(c, d, a, b, x[i + 14], 17, -1502002290);
+        b = md5_ff(b, c, d, a, x[i + 15], 22, 1236535329);
+
+        a = md5_gg(a, b, c, d, x[i + 1], 5, -165796510);
+        d = md5_gg(d, a, b, c, x[i + 6], 9, -1069501632);
+        c = md5_gg(c, d, a, b, x[i + 11], 14, 643717713);
+        b = md5_gg(b, c, d, a, x[i + 0], 20, -373897302);
+        a = md5_gg(a, b, c, d, x[i + 5], 5, -701558691);
+        d = md5_gg(d, a, b, c, x[i + 10], 9, 38016083);
+        c = md5_gg(c, d, a, b, x[i + 15], 14, -660478335);
+        b = md5_gg(b, c, d, a, x[i + 4], 20, -405537848);
+        a = md5_gg(a, b, c, d, x[i + 9], 5, 568446438);
+        d = md5_gg(d, a, b, c, x[i + 14], 9, -1019803690);
+        c = md5_gg(c, d, a, b, x[i + 3], 14, -187363961);
+        b = md5_gg(b, c, d, a, x[i + 8], 20, 1163531501);
+        a = md5_gg(a, b, c, d, x[i + 13], 5, -1444681467);
+        d = md5_gg(d, a, b, c, x[i + 2], 9, -51403784);
+        c = md5_gg(c, d, a, b, x[i + 7], 14, 1735328473);
+        b = md5_gg(b, c, d, a, x[i + 12], 20, -1926607734);
+
+        a = md5_hh(a, b, c, d, x[i + 5], 4, -378558);
+        d = md5_hh(d, a, b, c, x[i + 8], 11, -2022574463);
+        c = md5_hh(c, d, a, b, x[i + 11], 16, 1839030562);
+        b = md5_hh(b, c, d, a, x[i + 14], 23, -35309556);
+        a = md5_hh(a, b, c, d, x[i + 1], 4, -1530992060);
+        d = md5_hh(d, a, b, c, x[i + 4], 11, 1272893353);
+        c = md5_hh(c, d, a, b, x[i + 7], 16, -155497632);
+        b = md5_hh(b, c, d, a, x[i + 10], 23, -1094730640);
+        a = md5_hh(a, b, c, d, x[i + 13], 4, 681279174);
+        d = md5_hh(d, a, b, c, x[i + 0], 11, -358537222);
+        c = md5_hh(c, d, a, b, x[i + 3], 16, -722521979);
+        b = md5_hh(b, c, d, a, x[i + 6], 23, 76029189);
+        a = md5_hh(a, b, c, d, x[i + 9], 4, -640364487);
+        d = md5_hh(d, a, b, c, x[i + 12], 11, -421815835);
+        c = md5_hh(c, d, a, b, x[i + 15], 16, 530742520);
+        b = md5_hh(b, c, d, a, x[i + 2], 23, -995338651);
+
+        a = md5_ii(a, b, c, d, x[i + 0], 6, -198630844);
+        d = md5_ii(d, a, b, c, x[i + 7], 10, 1126891415);
+        c = md5_ii(c, d, a, b, x[i + 14], 15, -1416354905);
+        b = md5_ii(b, c, d, a, x[i + 5], 21, -57434055);
+        a = md5_ii(a, b, c, d, x[i + 12], 6, 1700485571);
+        d = md5_ii(d, a, b, c, x[i + 3], 10, -1894986606);
+        c = md5_ii(c, d, a, b, x[i + 10], 15, -1051523);
+        b = md5_ii(b, c, d, a, x[i + 1], 21, -2054922799);
+        a = md5_ii(a, b, c, d, x[i + 8], 6, 1873313359);
+        d = md5_ii(d, a, b, c, x[i + 15], 10, -30611744);
+        c = md5_ii(c, d, a, b, x[i + 6], 15, -1560198380);
+        b = md5_ii(b, c, d, a, x[i + 13], 21, 1309151649);
+        a = md5_ii(a, b, c, d, x[i + 4], 6, -145523070);
+        d = md5_ii(d, a, b, c, x[i + 11], 10, -1120210379);
+        c = md5_ii(c, d, a, b, x[i + 2], 15, 718787259);
+        b = md5_ii(b, c, d, a, x[i + 9], 21, -343485551);
+
+        a = safe_add(a, olda);
+        b = safe_add(b, oldb);
+        c = safe_add(c, oldc);
+        d = safe_add(d, oldd);
+    }
+    return Array(a, b, c, d);
+}
+
+/*
+ * Convert an array of little-endian words to a string
+ */
+function binl2rstr(input) {
+    var output = "";
+    for (var i = 0; i < input.length * 32; i += 8)
+        output += String.fromCharCode((input[i >> 5] >>> (i % 32)) & 0xFF);
+    return output;
+}
+
+
+function rstr_md5(s) {
+    return binl2rstr(binl_md5(rstr2binl(s), s.length * 8));
+}
+/*
+ * Encode a string as utf-8.
+ * For efficiency, this assumes the input is valid utf-16.
+ */
+function str2rstr_utf8(input) {
+    var output = "";
+    var i = -1;
+    var x, y;
+
+    while (++i < input.length) {
+        /* Decode utf-16 surrogate pairs */
+        x = input.charCodeAt(i);
+        y = i + 1 < input.length ? input.charCodeAt(i + 1) : 0;
+        if (0xD800 <= x && x <= 0xDBFF && 0xDC00 <= y && y <= 0xDFFF) {
+            x = 0x10000 + ((x & 0x03FF) << 10) + (y & 0x03FF);
+            i++;
+        }
+
+        /* Encode output as utf-8 */
+        if (x <= 0x7F)
+            output += String.fromCharCode(x);
+        else if (x <= 0x7FF)
+            output += String.fromCharCode(0xC0 | ((x >>> 6 ) & 0x1F),
+                0x80 | ( x & 0x3F));
+        else if (x <= 0xFFFF)
+            output += String.fromCharCode(0xE0 | ((x >>> 12) & 0x0F),
+                0x80 | ((x >>> 6 ) & 0x3F),
+                0x80 | ( x & 0x3F));
+        else if (x <= 0x1FFFFF)
+            output += String.fromCharCode(0xF0 | ((x >>> 18) & 0x07),
+                0x80 | ((x >>> 12) & 0x3F),
+                0x80 | ((x >>> 6 ) & 0x3F),
+                0x80 | ( x & 0x3F));
+    }
+    return output;
+}
+/*
+;angular.module('miaomiao.shop').factory('WeiChatPay', function ($http) {
+
+    if (!wx) {
+        console.error("微信js-sdk 没有加载");
+        return;
+    }
+
+    var weiChatPayUtils = {};
+    var wechatConfig = {
+        appId: '',
+        mch_id: ''
+    };
+
+    wx.config({
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: '', // 必填，公众号的唯一标识
+        timestamp: '', // 必填，生成签名的时间戳
+        nonceStr: '', // 必填，生成签名的随机串
+        signature: '',// 必填，签名，见附录1
+        jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    }).ready(function () {
+
+            // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+
+            weiChatPayUtils.checkPayAPI = function (success, fail) {
+                wx.checkJsApi({
+                    jsApiList: ['chooseWXPay'],
+                    success: function (res) {
+                        if (res.checkResult.chooseWXPay) {
+                            success();
+                        } else {
+                            fail();
+                        }
+                    },
+                    fail: function () {
+                        fail();
+                    }
+                });
+            };
+
+            weiChatPayUtils.getNonceStr = function () {
+                return Math.random().toString(36).substr(2, 15);
+            };
+
+            function objToString (obj) {
+                var str = '';
+                for (var p in obj) {
+                    if (obj.hasOwnProperty(p)) {
+                        str += p + '=' + obj[p] + '&';
+                    }
+                }
+                return str.slice(0,-1);
+            }
+
+            weiChatPayUtils.getHash = function (paramsArray) {
+                var params = paramsArray.sort(); // 默认字典序
+                var stringAllKeys = objToString(params);
+
+                //TODO: add key
+                var stringSignTemp= stringAllKeys + "&key=" + ''; //  our key
+
+                return hex_md5(stringSignTemp).toUpperCase();
+            };
+
+            weiChatPayUtils.getTimestamp = function () {
+                return parseInt(new Date().getTime() / 1000) + '';
+            };
+
+
+            //TODO: move this api call to server
+            weiChatPayUtils.unifiedorder = function (success, fail) {
+                var url = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
+
+                // see http://pay.weixin.qq.com/wiki/doc/api/index.php?chapter=9_1
+                var params = {
+                    appId: '', // 必填，公众号的唯一标识
+                    mch_id: '', //商户号
+                    nonce_str: '',
+                    sign: '',
+                    body: '',
+                    attach: '', // 附加数据，可以放置优惠券信息
+                    out_trade_no: '', //商户系统内部的订单号,32个字符内、可包含字母, 其他说明见商户订单号
+                    total_fee: '', // 订单总金额，只能为整数
+                    spbill_create_ip: '', //APP和网页支付提交用户端ip，Native支付填调用微信支付API的机器IP。
+                    notify_url: '',
+                    trade_type: 'JSAPI',
+                    openid: '' //trade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识
+                };
+
+                $http.post(url, params,
+                    {
+                        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+                    }).
+                    success(function (data, status, headers, config) {
+                        //return_code : SUCCESS/FAIL
+                        console.log(data);
+                        success(data, status, headers, config)
+                    }).
+                    error(function (data, status, headers, config) {
+                        fail(data, status, headers, config)
+                    });
+            };
+
+            weiChatPayUtils.chooseWXPay = function (success, fail) {
+
+                var info = {
+                    "appId": "wx2421b1c4370ec43b",
+                    timestamp: 0, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                    nonceStr: '', // 支付签名随机串，不长于 32 位
+                    package: '', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                    signType: 'MD5' // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                };
+
+                info.paySign = this.getHash(info); // 支付签名
+                info.success = function (res) {
+                    // 支付成功后的回调函数
+                    if (res.err_msg == "get_brand_wcpay_request:ok") {
+                    }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                    success(res);
+                };
+                wx.chooseWXPay(info);
+            };
+
+        }).error(function (res) {
+
+            // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+
+        });
+
+    return weiChatPayUtils;
+
+})
+;
+*/
