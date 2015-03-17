75fe6fe98c0b7eb62a04c48ce1eaab51fbaad138
allen2 <tianyixingkai@126.com>
2015-03-17 14:55:03 +0800

    mm

--- dev/jy/src/main/java/com/renren/ntc/sg/controllers/console/api/CommyController.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/controllers/console/api/CommyController.java
@@ -51,6 +51,17 @@ public class CommyController {
         return "@json:" + Constants.DONE ;
     }
 
+    @Get("del_link")
+    @Post("del_link")
+    public String del_link (Invocation inv,@Param("shop_id") long shop_id,  @Param("c_id") long c_id ){
+        if(!validate(shop_id,c_id)){
+            return "@json:" + Constants.PARATERERROR ;
+        }
+        shop_communityDao.del(shop_id,c_id);
+
+        return "@json:" + Constants.DONE ;
+    }
+
     private boolean validate(long shop_id, long c_id) {
        Shop shop =  shopDAO.getShop(shop_id);
         Community community =  communityDao.get(c_id);
