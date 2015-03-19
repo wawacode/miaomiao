e3ad1aa4c291846ba0f2c3416e1f7e3f30f6a065
jiliang <liangji101@yahoo.cn>
2015-03-19 19:55:18 +0800

    update pakcage info

--- dev/jy/src/main/webapp/sg/js/services/wxPayService.js
+++ dev/jy/src/main/webapp/sg/js/services/wxPayService.js
@@ -51,7 +51,7 @@ angular.module('miaomiao.shop').factory('WeiChatPay', function ($http, MMUtils, 
     };
 
     var config = {
-        'debug': true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+//        'debug': true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         'appId': wechatConfig.appId, // 必填，公众号的唯一标识
         'jsApiList': ['chooseWXPay']
     };
@@ -73,7 +73,6 @@ angular.module('miaomiao.shop').factory('WeiChatPay', function ($http, MMUtils, 
 
     wx.error(function (res) {
 
-//        MMUtils.showAlert('[DEBUG]微信JS-API 配置失败：' + JSON.stringify(res));
         // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
 
     });
