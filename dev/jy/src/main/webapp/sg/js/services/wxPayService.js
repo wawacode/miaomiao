af4d63e9ac5f395b213f3ebaf6f365ff5aebfdd2
jiliang <liangji101@yahoo.cn>
2015-03-22 15:17:32 +0800

    add log

--- dev/jy/src/main/webapp/sg/js/services/wxPayService.js
+++ dev/jy/src/main/webapp/sg/js/services/wxPayService.js
@@ -89,7 +89,9 @@ angular.module('miaomiao.shop').factory('WeiChatPay', function ($http, MMUtils, 
                 function onHashReady() {
 
                     info.success = function (res) {
+
                         // 支付成功后的回调函数
+                        window.alert('微信支付api返回:'+JSON.stringify(res));
 
                         if (res.errMsg == "chooseWXPay:ok") {
                             success(res.errMsg);
@@ -98,7 +100,7 @@ angular.module('miaomiao.shop').factory('WeiChatPay', function ($http, MMUtils, 
                         }    // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
                     };
 
-                    info.fail = function(){
+                    info.fail = function(res){
                         fail('支付失败:JS-API失败');
                     };
 
