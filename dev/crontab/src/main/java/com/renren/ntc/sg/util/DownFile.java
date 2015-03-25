afec5022954cf137762500cc75143936471dfc0d
allen2 <tianyixingkai@126.com>
2015-03-25 13:47:49 +0800

    mm

--- dev/crontab/src/main/java/com/renren/ntc/sg/util/DownFile.java
+++ dev/crontab/src/main/java/com/renren/ntc/sg/util/DownFile.java
@@ -1,5 +1,8 @@
 package com.renren.ntc.sg.util;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.renren.ntc.sg.bean.Item;
 import com.renren.ntc.sg.bean.Product;
 import com.renren.ntc.sg.biz.dao.ItemsDAO;
 import com.renren.ntc.sg.biz.dao.ProductDAO;
@@ -13,8 +16,6 @@ import java.net.URLConnection;
 public class DownFile {
 
     public DownFile() throws IOException {
-        InputStream is = new FileInputStream("product.txt");
-
     }
 
 
@@ -23,8 +24,48 @@ public class DownFile {
         RoseAppContext rose = new RoseAppContext();
         ItemsDAO itemDao = rose.getBean(ItemsDAO.class);
         ProductDAO pdDao = rose.getBean(ProductDAO.class);
+        String filePath = "F:\\product\\ylmt.txt";
+        readTxtFile(filePath,pdDao,itemDao);
 
 
+    }
+
+    public static void readTxtFile(String filePath,ProductDAO pdDao,ItemsDAO itemDao){
+        InputStreamReader read = null;
+        try {
+
+            String encoding="utf-16";
+            File file=new File(filePath);
+            if(file.isFile() && file.exists()){ //判断文件是否存在
+                read = new InputStreamReader(
+                        new FileInputStream(file),encoding);//考虑到编码格式
+                BufferedReader bufferedReader = new BufferedReader(read);
+                String lineTxt = null;
+                while((lineTxt = bufferedReader.readLine()) != null){
+                    String [] args = lineTxt.split("\t");
+                    if (null != args && args.length < 4) {
+                        System.out.println("drop " + lineTxt);
+                        continue;
+                    }
+                    System.out.println( lineTxt);
+                }
+            }else{
+                System.out.println("找不到指定的文件");
+            }
+
+        } catch (Exception e) {
+            System.out.println("读取文件内容出错");
+            e.printStackTrace();
+        }
+        finally{
+            if(null != read){
+                try {
+                    read.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 
