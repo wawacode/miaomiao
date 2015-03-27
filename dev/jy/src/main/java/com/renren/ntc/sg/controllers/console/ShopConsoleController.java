9702a6b8bf588d8a71ab4f9790b0557c979b5162
leehom316 <chunhai.li@lichunhai-PC.qunarservers.com>
2015-03-27 15:17:38 +0800

    提交订单聚合列表代码

--- dev/jy/src/main/java/com/renren/ntc/sg/controllers/console/ShopConsoleController.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/controllers/console/ShopConsoleController.java
@@ -1,12 +1,32 @@
 package com.renren.ntc.sg.controllers.console;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.paoding.rose.web.Invocation;
+import net.paoding.rose.web.annotation.Param;
+import net.paoding.rose.web.annotation.Path;
+import net.paoding.rose.web.annotation.rest.Get;
+import net.paoding.rose.web.annotation.rest.Post;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.renren.ntc.sg.annotations.AuthorizeCheck;
 import com.renren.ntc.sg.annotations.DenyCommonAccess;
 import com.renren.ntc.sg.annotations.LoginRequired;
-import com.renren.ntc.sg.bean.*;
+import com.renren.ntc.sg.bean.Category;
+import com.renren.ntc.sg.bean.Item;
+import com.renren.ntc.sg.bean.Order;
+import com.renren.ntc.sg.bean.Shop;
+import com.renren.ntc.sg.bean.ShopCategory;
 import com.renren.ntc.sg.biz.dao.CategoryDAO;
 import com.renren.ntc.sg.biz.dao.ItemsDAO;
 import com.renren.ntc.sg.biz.dao.OrdersDAO;
@@ -16,29 +36,10 @@ import com.renren.ntc.sg.constant.SgConstant;
 import com.renren.ntc.sg.interceptors.access.RegistHostHolder;
 import com.renren.ntc.sg.service.LoggerUtils;
 import com.renren.ntc.sg.service.OrderService;
-import com.renren.ntc.sg.service.RegistUserService;
 import com.renren.ntc.sg.util.Constants;
 import com.renren.ntc.sg.util.FileUploadUtils;
 import com.renren.ntc.sg.util.ImagesUtils;
 import com.renren.ntc.sg.util.SUtils;
-import net.paoding.rose.web.Invocation;
-import net.paoding.rose.web.annotation.Param;
-import net.paoding.rose.web.annotation.Path;
-import net.paoding.rose.web.annotation.rest.Get;
-import net.paoding.rose.web.annotation.rest.Post;
-
-import org.apache.commons.lang.NumberUtils;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 
