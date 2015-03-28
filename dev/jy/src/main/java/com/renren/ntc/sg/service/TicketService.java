20db520b76e13eb3aa99f935923d1bb95e2b29fa
allen <allen@allen-Aspire-4736.(none)>
2015-03-28 23:37:10 +0800

    mm

---
+++ dev/jy/src/main/java/com/renren/ntc/sg/service/TicketService.java
@@ -0,0 +1,94 @@
+package com.renren.ntc.sg.service;
+
+import com.renren.ntc.sg.bean.User;
+import com.renren.ntc.sg.bean.UserCoupon;
+import com.renren.ntc.sg.biz.dao.UserCouponDAO;
+import com.renren.ntc.sg.biz.dao.UserDAO;
+import com.renren.ntc.sg.jredis.JRedisUtil;
+import com.renren.ntc.sg.util.Constants;
+import com.renren.ntc.sg.util.SUtils;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yunming.zhu
+ * Date: 14-12-8
+ * Time: 下午12:09
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class TicketService {
+
+
+    @Autowired
+    public UserCouponDAO userCouponDao;
+
+    public void occupy(long coupon_id, String coupon_code) {
+        String key = SUtils.ticketKey(coupon_id, coupon_code);
+        JRedisUtil.getInstance().set(key, Constants.COUPONTRADE );
+        JRedisUtil.getInstance().expire(key,Constants.TRADEPIE);
+    }
+
+    public boolean canOcupy(long coupon_id, String coupon_code) {
+        String key = SUtils.ticketKey(coupon_id, coupon_code);
+        String  value = JRedisUtil.getInstance().get(key);
+        if (StringUtils.isBlank(value)){
+            return true;
+        }
+        return false;
+    }
+
+    public UserCoupon getTicket(long user_id,long coupon_id, String coupon_code){
+        UserCoupon ticket =  userCouponDao.getTicket(user_id, coupon_id, coupon_code, Constants.COUPONUNUSED);
+        if(ticket != null && canOcupy(ticket.getId(),ticket.getCode()))
+        {
+            return ticket;
+        }
+        return null;
+    }
+
+    public boolean writeoff(long coupon_id, String coupon_code) {
+        String key = SUtils.ticketKey(coupon_id, coupon_code);
+        String  value = JRedisUtil.getInstance().get(key);
+        if (StringUtils.isBlank(key)){
+            return true;
+        }
+        return false;
+    }
+
+
+    public  boolean usedTicket (long user_id,long shop_id) {
+        String key = SUtils.generDaylimitTicketKey(user_id);
+        long value = JRedisUtil.getInstance().incr(key);
+        if (value == 1L){
+            return true;
+        }
+        return false;
+    }
+
+    public  List<UserCoupon> getUnusedTickets (long user_id,long shop_id) {
+        String key = SUtils.generDaylimitTicketKey(user_id);
+        List<UserCoupon> tt = new ArrayList<UserCoupon>();
+        long value = JRedisUtil.getInstance().incr(key);
+        if (value == 1L){
+            List<UserCoupon>  tickets = userCouponDao.geShopCoupons(user_id,shop_id,Constants.COUPONUNUSED);
+            for(UserCoupon t: tickets){
+              LoggerUtils.getInstance().log(String.format(" check ocupy user %d , ticket id %d , code %s ", user_id, t.getId(), t.getCode()));
+              if (canOcupy(t.getId(),t.getCode())){
+                  tt.add(t);
+              }
+          }
+        }
+        return tt;
+    }
+
+
+
+}
