eb40cae84b1de3e7ab65f644f5ca120f01ead3d4
allen <allen@allen-Aspire-4736.(none)>
2015-03-30 23:32:58 +0800

    m

---
+++ dev/jy/src/main/java/com/renren/ntc/sg/util/NotifyOrder.java
@@ -0,0 +1,51 @@
+package com.renren.ntc.sg.util;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import com.renren.ntc.sg.jredis.JRedisUtil;
+import com.renren.ntc.sg.service.WXService;
+import net.paoding.rose.scanning.context.RoseAppContext;
+import org.apache.commons.lang.StringUtils;
+
+public class NotifyOrder {
+    //private l
+    public static void main(String[] args) {
+        RoseAppContext rose = new RoseAppContext();
+        WXService wxservice = rose.getBean(WXService.class);
+        Set<String> orderInfo = JRedisUtil.getInstance().smembers(Constants.ORDER_KEY);
+        for (Iterator iterator = orderInfo.iterator(); iterator.hasNext();) {
+            String orderinfo = (String) iterator.next();
+            System.out.println("get " + orderinfo);
+            if(StringUtils.isBlank(orderinfo)){
+                continue;
+            }
+            String[] orderInfoArr = orderinfo.split("_");
+            if(orderInfoArr == null || orderInfoArr.length !=3){
+                JRedisUtil.getInstance().srem(Constants.ORDER_KEY,orderinfo);
+                continue;
+            }
+            if(StringUtils.isBlank(orderInfoArr[0])){
+                JRedisUtil.getInstance().srem(Constants.ORDER_KEY,orderinfo);
+                continue;
+            }
+            try {
+                String order_id = orderInfoArr[0];
+                long s_id = Long.valueOf(orderInfoArr[1]);
+                long redisTime = Long.parseLong(orderInfoArr[2]);
+                long curr = System.currentTimeMillis();
+                long left = curr - redisTime;
+                if (left > 300000){
+                    wxservice.sendWX2User(order_id,s_id);
+                    JRedisUtil.getInstance().srem(Constants.ORDER_KEY, orderinfo);
+                    continue;
+                }
+            }catch (Exception e){
+                 e.printStackTrace();
+                JRedisUtil.getInstance().srem(Constants.ORDER_KEY, orderinfo);
+                continue;
+            }
+
+        }
+    }
+}
