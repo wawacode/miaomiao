c8308a806455989ace5b058e8edf6fc8367bd646
allen <allen@allen-Aspire-4736.(none)>
2015-03-30 22:56:38 +0800

    m

--- dev/jy/src/main/java/com/renren/ntc/sg/biz/dao/UserCouponDAO.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/biz/dao/UserCouponDAO.java
@@ -22,32 +22,32 @@ public interface UserCouponDAO {
     static final String  FIELDS = "id,user_id,coupon_id,shop_id,pic_url,code,status, price ,name  ,ext,start_time,end_time,create_time,update_time"  ;
     static final String  INSERT_FIELDS = "user_id,coupon_id,shop_id,pic_url,code ,status, price ,name,ext,start_time,end_time"  ;
 
-    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  user_id =:1 and status = 0 order by create_time desc limit :2,:3 ")
-    public List<UserCoupon> getUser_Coupon (long user_id , int form ,int offset) ;
+    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  user_id =:1 and status = 0 and start_time <= :4  and  end_time >= :4  order by create_time desc limit :2,:3 ")
+    public List<UserCoupon> getUser_Coupon (long user_id , int form ,int offset,Date now) ;
 
-    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  user_id =:1 and status=:2 order by create_time desc limit :3,:4 ")
-    public List<UserCoupon> getMyCoupon (long user_id ,long status,  int form ,int offset) ;
+    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  user_id =:1 and status=:2  and start_time <= :5  and  end_time >= :5  order by create_time desc limit :3,:4 ")
+    public List<UserCoupon> getMyCoupon (long user_id ,long status,  int form ,int offset , Date now) ;
 
     @SQL("insert into " + TABLE_NAME + " ( " + INSERT_FIELDS + ") values (:1.user_id,:1.coupon_id,:1.shop_id,:1.pic_url,:1.code ,:1.status,:1.price ," +
             ":1.name ,:1.ext,:1.start_time,:1.end_time) ")
     public int insert ( UserCoupon userCoupon) ;
 
 
-    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  id =:1 and code = :2")
-    public List<UserCoupon> getUser_Coupon(long user_id, long id, String code) ;
+    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  id =:1 and code = :2 and start_time <= :4  and  end_time >= :4 ")
+    public List<UserCoupon> getUser_Coupon(long user_id, long id, String code ,Date now) ;
 
 
-    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  user_id =:1 and coupon_id = :2")
-    public List<UserCoupon> getUser_Coupon (long user_id, long coupon_id ) ;
+    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  user_id =:1 and coupon_id = :2 and start_time <= :3  and  end_time >= :3 ")
+    public List<UserCoupon> getUser_Coupon (long user_id, long coupon_id ,Date now) ;
 
 
-    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  user_id =:1 and shop_id = :2 and status= :3  limit :4,:5 ")
-    public List<UserCoupon> geShopCoupons (long user_id, long shop_id ,int status ,int from,int offset ) ;
+    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  user_id =:1 and shop_id = :2 and status= :3  and start_time <= :6  and  end_time >= :6  limit :4,:5 ")
+    public List<UserCoupon> geShopCoupons (long user_id, long shop_id ,int status ,int from,int offset ,Date now) ;
 
 
 
-    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  user_id =:1 and id = :2 and code=:3 and status = :4")
-    public UserCoupon getTicket(long user_id, long coupon_id, String coupon_code, int couponunused);
+    @SQL("select " + FIELDS + " from " + TABLE_NAME +" where  user_id =:1 and id = :2 and code=:3 and status = :4 and start_time <= :5  and  end_time >= :5 ")
+    public UserCoupon getTicket(long user_id, long coupon_id, String coupon_code, int couponunused,Date now);
 
 
 
@@ -57,6 +57,6 @@ public interface UserCouponDAO {
 
 
     @SQL("select count(*) from " + TABLE_NAME +" where  user_id =:1 and status=:2 and start_time <= :3  and  end_time >= :3 ")
-    public int getMyCouponCount(long user_id , int status,Date now);
-    
+    public int getMyCouponCount(long user_id , int status, Date now);
+
 }
