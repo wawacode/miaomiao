061d280cf271503c31245786bce0aef48cb041f4
allen <allen@allen-Aspire-4736.(none)>
2015-03-31 21:31:27 +0800

    remove coupon enable shop

--- dev/jy/src/main/webapp/sg/js/services/services.js
+++ dev/jy/src/main/webapp/sg/js/services/services.js
@@ -322,7 +322,7 @@ angular.module('miaomiao.shop').factory('httpClient', ['$http', function ($http)
             },
 
             isCouponEnabledShop:function(shop){
-                return shop.id == '10033';
+                return true; // shop.id == '10033';
             }
         }
     }]).factory('MMUtils', ['$timeout', '$ionicLoading', '$ionicPopup', function ($timeout,$ionicLoading, $ionicPopup) {
