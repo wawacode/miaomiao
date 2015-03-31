ba768026573c8c140a2f94fbe1ce9449d3cbfc5e
allen <allen@allen-Aspire-4736.(none)>
2015-03-31 22:30:08 +0800

    mm

--- dev/jy/src/main/java/com/renren/ntc/sg/controllers/wx/WXController.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/controllers/wx/WXController.java
@@ -3,6 +3,7 @@ package com.renren.ntc.sg.controllers.wx;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.renren.ntc.sg.annotations.DenyCommonAccess;
 import com.renren.ntc.sg.bean.Device;
 import com.renren.ntc.sg.bean.Shop;
 import com.renren.ntc.sg.biz.dao.*;
@@ -26,6 +27,7 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Path("")
+@DenyCommonAccess
 public class WXController {
 
     @Autowired
