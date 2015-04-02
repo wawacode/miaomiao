a17804a761e32e9ff056af04ebd9e273d138a7ae
leehom316 <chunhai.li@lichunhai-PC.qunarservers.com>
2015-04-02 17:25:50 +0800

    commit

--- dev/jy/src/main/java/com/renren/ntc/sg/util/OrderDetailUtil.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/util/OrderDetailUtil.java
@@ -11,6 +11,8 @@ import org.apache.commons.lang.math.NumberUtils;
 import com.alibaba.fastjson.JSONObject;
 import com.renren.ntc.sg.bean.Order;
 import com.renren.ntc.sg.bean.Shop;
+import com.renren.ntc.sg.bean.WXPayShopReport;
+import com.renren.ntc.sg.bean.WXPayShopReport.WXPayDetail;
 import com.renren.ntc.sg.biz.dao.OrdersDAO;
 import com.renren.ntc.sg.biz.dao.ShopDAO;
 import com.renren.ntc.sg.mail.MailSendInfo;
@@ -30,7 +32,7 @@ public class OrderDetailUtil {
 		List<Shop> shops = shopDAO.getAllShopsByAudit(1);
 		WXPayShopReport wShopReport = null;
 		WXPayDetail  wxpDetail = null;
-		List<WXPayShopReport> wxpayShopReports = new ArrayList<OrderDetailUtil.WXPayShopReport>();
+		List<WXPayShopReport> wxpayShopReports = new ArrayList<WXPayShopReport>();
 		for(Shop shop : shops){
 			System.out.println("shopid="+shop.getId()+",name="+shop.getName());
 			String beginTimeStr = Dateutils.tranferDate2Str(Dateutils.getDateByCondition(-1, 0, 0, 0));
@@ -44,7 +46,7 @@ public class OrderDetailUtil {
 			wShopReport.setReportDate(Dateutils.tranferDefaultDate2Str(Dateutils.getDateByCondition(-1, 23, 0, 0)));
 			List<WXPayDetail> wxpDetails = new ArrayList<WXPayDetail>();
 			for(Order order : orders){
-				wxpDetail = new WXPayDetail();
+				wxpDetail = wShopReport.new WXPayDetail();
 				wxpDetail.setOrderPrice(order.getPrice()/100);
 				String msg = order.getMsg();
 				int wxDiscount = 0;
@@ -84,6 +86,7 @@ public class OrderDetailUtil {
 					 
     String orderInfoHtml = "";
 	for(WXPayShopReport wxPayShopReport : wxpPayShopReports){
+		System.out.println("====>"+wxPayShopReport.getShopName());
 		List<WXPayDetail> shopOrderFlows = wxPayShopReport.getShopOrderFlows();
 		int rowspan = shopOrderFlows.size() + 1;
 		orderInfoHtml = orderInfoHtml + "<tr> <td rowspan='"+rowspan+"'>"+wxPayShopReport.getShopId()+"</td> <td rowspan='"+rowspan+"'>"+wxPayShopReport.getShopName()+"</td> <td rowspan='"+rowspan+"'>"+wxPayShopReport.getReportDate()+"</td> <td rowspan='"+rowspan+"'>"+wxPayShopReport.getOrderCount()+"</td>";
@@ -94,76 +97,5 @@ public class OrderDetailUtil {
 	}
 	orderInfoHtml = orderInfoHtml + "</tbody> </table> </body> </html>";
 	return html + orderInfoHtml;
-	}
-	
-	public static class WXPayShopReport{
-		private long shopId;
-		private String shopName;
-		private int orderCount;
-		private String reportDate;
-		private List<WXPayDetail> shopOrderFlows;
-		public WXPayShopReport(){
-			
-		}
-		public long getShopId() {
-			return shopId;
-		}
-		public void setShopId(long shopId) {
-			this.shopId = shopId;
-		}
-		public String getShopName() {
-			return shopName;
-		}
-		public void setShopName(String shopName) {
-			this.shopName = shopName;
-		}
-		public int getOrderCount() {
-			return orderCount;
-		}
-		public void setOrderCount(int orderCount) {
-			this.orderCount = orderCount;
-		}
-		public String getReportDate() {
-			return reportDate;
-		}
-		public void setReportDate(String reportDate) {
-			this.reportDate = reportDate;
-		}
-		public List<WXPayDetail> getShopOrderFlows() {
-			return shopOrderFlows;
-		}
-		public void setShopOrderFlows(List<WXPayDetail> shopOrderFlows) {
-			this.shopOrderFlows = shopOrderFlows;
-		}
-	}
-	public static class WXPayDetail{
-		private int orderPrice;
-		private int wxDiscount;
-		private int realPrice;
-		private String orderTimeStr;
-		public int getOrderPrice() {
-			return orderPrice;
-		}
-		public void setOrderPrice(int orderPrice) {
-			this.orderPrice = orderPrice;
-		}
-		public int getWxDiscount() {
-			return wxDiscount;
-		}
-		public void setWxDiscount(int wxDiscount) {
-			this.wxDiscount = wxDiscount;
-		}
-		public int getRealPrice() {
-			return realPrice;
-		}
-		public void setRealPrice(int realPrice) {
-			this.realPrice = realPrice;
-		}
-		public String getOrderTimeStr() {
-			return orderTimeStr;
-		}
-		public void setOrderTimeStr(String orderTimeStr) {
-			this.orderTimeStr = orderTimeStr;
-		}
 	}
 }
