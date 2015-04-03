e43752d19bb8a9dcda89d6387387b118af756202
liangdaxian <liangji101@yahoo.com>
2015-04-03 15:38:54 +0800

    add debug log

--- dev/jy/src/main/webapp/sg/js/services/wxPayService.js
+++ dev/jy/src/main/webapp/sg/js/services/wxPayService.js
@@ -26,6 +26,8 @@ angular.module('miaomiao.shop').factory('WeiChatPay', function ($http, MMUtils, 
         config.nonceStr = wpconfig.nonceStr;
         config.timestamp = wpconfig.timestamp;
 
+        MMUtils.showAlert('微信 pay config...');
+
         wx.config(config);
 
         wx.error(function (res) {
@@ -57,6 +59,14 @@ angular.module('miaomiao.shop').factory('WeiChatPay', function ($http, MMUtils, 
     };
 
     weiChatPayUtils.chooseWXPay = function (info, beforeHandoverToWCPay, success, fail) {
+
+        var config = {
+            'debug': true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            'appId': wechatConfig.appId, // 必填，公众号的唯一标识
+            'jsApiList': ['chooseWXPay']
+        };
+
+        info.appId = config.appId;
 
         info.success = function (res) {
 
