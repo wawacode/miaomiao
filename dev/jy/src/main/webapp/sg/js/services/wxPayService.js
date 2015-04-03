7ae9070c90389c897880aff7c18ba5d986625ac9
liangdaxian <liangji101@yahoo.com>
2015-04-03 15:56:27 +0800

    remove debug log

--- dev/jy/src/main/webapp/sg/js/services/wxPayService.js
+++ dev/jy/src/main/webapp/sg/js/services/wxPayService.js
@@ -17,7 +17,7 @@ angular.module('miaomiao.shop').factory('WeiChatPay', function ($http, MMUtils, 
     weiChatPayUtils.initConfig = function(wpconfig){
         // init the api
         var config = {
-            'debug': true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+//            'debug': true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             'appId': wechatConfig.appId, // 必填，公众号的唯一标识
             'jsApiList': ['chooseWXPay']
         };
@@ -54,13 +54,7 @@ angular.module('miaomiao.shop').factory('WeiChatPay', function ($http, MMUtils, 
         });
     };
 
-    weiChatPayUtils.chooseWXPay = function (info, beforeHandoverToWCPay, success, fail) {
-
-        var config = {
-            'debug': true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            'appId': wechatConfig.appId, // 必填，公众号的唯一标识
-            'jsApiList': ['chooseWXPay']
-        };
+    weiChatPayUtils.chooseWXPay = function (info, success, fail) {
 
         info.success = function (res) {
 
