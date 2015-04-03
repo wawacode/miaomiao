865b30483114e09a0ae39d6f936d3d41928d9de2
leehom316 <chunhai.li@lichunhai-PC.qunarservers.com>
2015-04-03 17:15:43 +0800

    commit

--- dev/crontab/src/main/java/com/renren/ntc/sg/util/DeleteShopItems.java
+++ dev/crontab/src/main/java/com/renren/ntc/sg/util/DeleteShopItems.java
@@ -5,13 +5,22 @@ import net.paoding.rose.scanning.context.RoseAppContext;
 import com.renren.ntc.sg.biz.dao.ItemsDAO;
 
 public class DeleteShopItems {
-
-    private static long shop_id = 10060;
-
-    //private static long shop[] = { 10044, 10058, 10053, 10048, 10049, 10051, 10041, 10059, 10043, 10042, 
-    //10047, 10045, 10062, 10052, 10034, 10060, 10046, 10057, 10056 };
-    private static long shop[] = { 10073 };
-
+	/**
+	 * +-------+----------------------------------+
+| 10067 |              测试圣馨便利点-勿动 |
+| 10069 |             测试-方舟苑测试-勿动 |
+| 10070 |             测试-鑫世纪华联-勿动 |
+| 10071 |                 测试-福奈特-勿动 |
+| 10072 |           测试-望京西园二区-勿动 |
+| 10075 |               测试-京诚超市-勿动 |
+| 10076 |             测试-大西洋新城-勿动 |
+| 10077 |                测试-108测试-勿动 |
+| 10078 |                 测试-花家地-勿动 |
+| 10079 |               测试-金隅立港-勿动 |
+| 10080 |                    测试-219-勿动 |
+	 */
+	
+    private static long shop[] = { 10067,10069, 10070,10071,10072,10075,10076,10077,10078,10079,10080};
     public static void main(String[] args) {
         deleteAllItems();
     }
@@ -27,17 +36,17 @@ public class DeleteShopItems {
         System.out.println("删除完成");
     }
 
-    private static void deletePriceZero() {
-        RoseAppContext rose = new RoseAppContext();
-        ItemsDAO itemDao = rose.getBean(ItemsDAO.class);
-        itemDao.del(SUtils.generTableName(shop_id), shop_id);
-        System.out.println("删除完成");
-    }
-
-    private static void deleteCategory() {
-        RoseAppContext rose = new RoseAppContext();
-        ItemsDAO itemDao = rose.getBean(ItemsDAO.class);
-        itemDao.delCategory(SUtils.generTableName(shop_id), shop_id, 15);
-        System.out.println("删除完成");
-    }
+//    private static void deletePriceZero() {
+//        RoseAppContext rose = new RoseAppContext();
+//        ItemsDAO itemDao = rose.getBean(ItemsDAO.class);
+//        itemDao.del(SUtils.generTableName(shop_id), shop_id);
+//        System.out.println("删除完成");
+//    }
+//
+//    private static void deleteCategory() {
+//        RoseAppContext rose = new RoseAppContext();
+//        ItemsDAO itemDao = rose.getBean(ItemsDAO.class);
+//        itemDao.delCategory(SUtils.generTableName(shop_id), shop_id, 15);
+//        System.out.println("删除完成");
+//    }
 }
