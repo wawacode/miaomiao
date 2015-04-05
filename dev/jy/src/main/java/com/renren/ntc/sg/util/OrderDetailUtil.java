fd5be13a1cf51e0ad406a579cab5307621b2d817
leehom316 <chunhai.li@lichunhai-PC.qunarservers.com>
2015-04-05 13:38:32 +0800

    commit

--- dev/jy/src/main/java/com/renren/ntc/sg/util/OrderDetailUtil.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/util/OrderDetailUtil.java
@@ -33,6 +33,7 @@ public class OrderDetailUtil {
 		OrdersDAO orderDao = rose.getBean(OrdersDAO.class);
 		List<Shop> shops = shopDAO.getAllShopsByAudit(1);
 		List<WXPayShopReport> wxpayShopReports = new ArrayList<WXPayShopReport>();
+		int totalShopOrderPrice = 0;
 		for(Shop shop : shops){
 			//System.out.println("shopid="+shop.getId()+",name="+shop.getName());
 			String beginTimeStr = Dateutils.tranferDate2Str(Dateutils.getDateByCondition(dateInt, 0, 0, 0));
@@ -72,6 +73,7 @@ public class OrderDetailUtil {
 				wxpDetail.setOrderTimeStr(Dateutils.tranferDate2Str(order.getCreate_time()));
 				wxpDetails.add(wxpDetail);	
 			}
+			totalShopOrderPrice += totalPrice;
 			wShopReport.setTotalPrice((float)totalPrice/100);
 			wShopReport.setShopOrderFlows(wxpDetails);
 			wxpayShopReports.add(wShopReport);
@@ -80,27 +82,39 @@ public class OrderDetailUtil {
 		}
 		MailSendServer mailSendServer = com.renren.ntc.sg.mail.MailSendServer.getServer("smtp.163.com", 25, "lee_yeah1@163.com", "Lee1qaz2wsx",
 	              "lee_yeah1@163.com");
-		  mailSendServer.sendTextInfo(new MailSendInfo("喵喵微信支付每日订单详情",getHtmlInfo(wxpayShopReports), mailLists, new String[0]),false);
+		  mailSendServer.sendTextInfo(new MailSendInfo("喵喵微信支付每日订单详情",getHtmlInfo(wxpayShopReports,totalShopOrderPrice), mailLists, new String[0]),false);
 	}
 	
-	private static String getHtmlInfo(List<WXPayShopReport> wxpPayShopReports){
+	private static String getHtmlInfo(List<WXPayShopReport> wxpPayShopReports,int totalShopOrderPrice){
 		String html = "<html><head><title></title></head><body>"
 					 + "<table border='1' cellpadding='1' cellspacing='1' style='width: 1000px;'>"
-					 + "<tbody> <tr> <td> 店铺ID</td> <td> 店铺名称</td> <td> 订单报告日期</td> <td> 微信订单总数</td><td> 微信订单总额</td> <td colspan='4' align='center'> 每笔订单详情</td> </tr>";
+					 + "<tbody> <tr> <td> 店铺ID</td> <td> 店铺名称</td> <td> 订单报告日期</td> <td> 微信订单总数</td><td> 微信订单总额</td> <td colspan='4' align='center'> 每笔订单详情</td><td> 总计(元)</td> </tr>";
 					 
     String orderInfoHtml = "";
+    String totalShopOrderPriceStr = (float)totalShopOrderPrice/100+"";
+    int loopCount =1;
+    int i=0;
+    int totalRowSpan=0;
 	for(WXPayShopReport wxPayShopReport : wxpPayShopReports){
+		i++;
 		System.out.println("====>"+wxPayShopReport.getShopName());
 		List<WXPayDetail> shopOrderFlows = wxPayShopReport.getShopOrderFlows();
 		int rowspan = shopOrderFlows.size() + 1;
+		totalRowSpan +=rowspan;
 		orderInfoHtml = orderInfoHtml + "<tr> <td rowspan='"+rowspan+"'>"+wxPayShopReport.getShopId()+"</td> <td rowspan='"+rowspan+"'>"+wxPayShopReport.getShopName()+"</td> <td rowspan='"+rowspan+"'>"+wxPayShopReport.getReportDate()+"</td> <td rowspan='"+rowspan+"'>"+wxPayShopReport.getOrderCount()+"</td>"
 				                      +"<td rowspan='"+rowspan+"'>"+wxPayShopReport.getTotalPrice()+"元"+"</td>";
-		orderInfoHtml = orderInfoHtml + "<td> 订单时间</td> <td> 下单金额(元)</td> <td> 优惠券金额(元)</td> <td> 最终金额(元)</td> </tr>";
+		if(loopCount == i){
+			orderInfoHtml = orderInfoHtml + "<td> 订单时间</td> <td> 下单金额(元)</td> <td> 优惠券金额(元)</td> <td> 最终金额(元)</td><td rowspan='$1'>"+totalShopOrderPriceStr+"</td></tr>";
+		}else {
+			orderInfoHtml = orderInfoHtml + "<td> 订单时间</td> <td> 下单金额(元)</td> <td> 优惠券金额(元)</td> <td> 最终金额(元)</td> </tr>";
+		}
+		
 		for(WXPayDetail wxpayDetail : shopOrderFlows){
 			orderInfoHtml = orderInfoHtml + "<tr><td>"+wxpayDetail.getOrderTimeStr()+"</td><td>"+wxpayDetail.getOrderPrice()+"</td> <td>"+wxpayDetail.getWxDiscount()+"</td> <td>"+wxpayDetail.getRealPrice()+"</td></tr>";
 		}
 	}
 	orderInfoHtml = orderInfoHtml + "</tbody> </table> </body> </html>";
+	orderInfoHtml = orderInfoHtml.replace("$1", totalRowSpan+"");
 	return html + orderInfoHtml;
 	}
 }
