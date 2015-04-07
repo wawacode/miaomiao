117195d01ecf52eb14a50e8130c1cfafc000452b
allen2 <tianyixingkai@126.com>
2015-04-07 21:19:05 +0800

    mm

--- dev/jy/src/main/java/com/renren/ntc/sg/controllers/wx/WXController.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/controllers/wx/WXController.java
@@ -170,6 +170,9 @@ public class WXController {
                 }
 
                 Order order = orderDao.getOrder(order_id,SUtils.generOrderTableName(shop_id));
+                if(done(order)){
+                    return "@"+PAY_REPAY;
+                }
                 orderDao.paydone(Constants.ORDER_WAIT_FOR_PRINT,order_id,SUtils.generOrderTableName(shop_id));
                 userOrdersDAO.paydone(Constants.ORDER_WAIT_FOR_PRINT,order_id,SUtils.generUserOrderTableName(user_id));
                 if( coupon_id != 0) {
