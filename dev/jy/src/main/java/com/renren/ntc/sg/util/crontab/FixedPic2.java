0e39a3d3879675c8e3e2eb4171a79bf841eb55bf
allen2 <tianyixingkai@126.com>
2015-04-14 22:10:16 +0800

    mm

--- dev/jy/src/main/java/com/renren/ntc/sg/util/crontab/FixedPic2.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/util/crontab/FixedPic2.java
@@ -32,7 +32,10 @@ public class FixedPic2 {
         File f = new File(PATH );
         File file[]  = f.listFiles();
         for (File ff : file)  {
-            System.out.println(ff.getName());
+            String serialNo = ff.getName().split(".")[0];
+            String pic_url = "http://www.mbianli.com/cat/images/shop_" + shop_id + "/" + ff.getName();
+            System.out.println("update " + serialNo+" " +  pic_url);
+            itemDao.updateforSerialNo(SUtils.generTableName(shop_id),pic_url,serialNo);
         }
     }
 
