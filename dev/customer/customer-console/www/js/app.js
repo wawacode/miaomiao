4f13ef7b5aa2f8c40556c192f483709e9f16f74a
liangdaxian <liangji101@yahoo.com>
2015-04-15 15:59:40 +0800

    change to mbianli api

--- dev/customer/customer-console/www/js/app.js
+++ dev/customer/customer-console/www/js/app.js
@@ -15,8 +15,8 @@ angular.module('miaomiao.console', [
     ])
 
     // for server config
-//    .constant('serverInfo', {host: 'http://www.mbianli.com:8088', context: '/console/api/'})
-    .constant('serverInfo', {host: 'http://127.0.0.1:8010', context: '/console/api/'})
+    .constant('serverInfo', {host: 'http://www.mbianli.com:8088', context: '/console/api/'})
+//    .constant('serverInfo', {host: 'http://127.0.0.1:8010', context: '/console/api/'})
 
     // for http post requests
     .config(function ($httpProvider, $provide) {
