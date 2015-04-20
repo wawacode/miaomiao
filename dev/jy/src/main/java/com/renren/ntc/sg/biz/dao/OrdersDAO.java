688014a5cebeeaff9b04d3a91ad170aa88421e0d
allen <allen@allen-Aspire-4736.(none)>
2015-04-20 18:00:33 +0800

    mm

--- dev/jy/src/main/java/com/renren/ntc/sg/biz/dao/OrdersDAO.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/biz/dao/OrdersDAO.java
@@ -27,19 +27,11 @@ CREATE TABLE `items` (
 
 @DAO(catalog = "ABC")
 public interface OrdersDAO {
-<<<<<<< HEAD
     static final String FIELDS = "id, order_id,readed,shop_id,user_id,address_id,remarks ,act,msg,info ,snapshot,status,price,create_time,update_time,order_status,order_info,user_confirm_time" ;
     static final String INSERT_FIELDS = " order_id,readed,shop_id,user_id,address_id,remarks ,act, info,snapshot,status,price" ;
     
     @SQL("select "+ FIELDS +" from ##(:tableName)   where shop_id =:1 and ( status =1 or status = 2) order by create_time desc limit :2,:3")
     public List<Order> getOrderByShop(long shop_id, int start, int offset,@SQLParam("tableName") String tableName);
-=======
-    static final String FIELDS = "id, order_id,readed,shop_id,user_id,address_id,remarks ,act,msg,info ,snapshot,status,price,create_time,update_time,order_status,order_info";
-    static final String INSERT_FIELDS = " order_id,readed,shop_id,user_id,address_id,remarks ,act, info,snapshot,status,price";
-
-    @SQL("select " + FIELDS + " from ##(:tableName)   where shop_id =:1 and ( status =1 or status = 2) order by create_time desc limit :2,:3")
-    public List<Order> getOrderByShop(long shop_id, int start, int offset, @SQLParam("tableName") String tableName);
->>>>>>> phil
 
 
     @SQL("insert into  ##(:tableName) (" + INSERT_FIELDS + ") values(:1.order_id,:1.readed,:1.shop_id," +
