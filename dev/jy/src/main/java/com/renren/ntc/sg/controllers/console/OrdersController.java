26f2140d46dee212eb8774fae4d5410fa6ede264
super-phil <zhaoxiufei@gmail.com>
2015-04-20 17:16:35 +0800

    添加查询框

--- dev/jy/src/main/java/com/renren/ntc/sg/controllers/console/OrdersController.java
+++ dev/jy/src/main/java/com/renren/ntc/sg/controllers/console/OrdersController.java
@@ -70,6 +70,7 @@ public class OrdersController {
         orderls = orderService.forV(orderls);
         List<OrderDetail> orderDetails = orderService.setOrderDetail(orderls);
         inv.addModel("shop_id", 1);
+        inv.addModel("shops",shopList);
         inv.addModel("orderls", orderDetails);
         System.out.println(orderls.size() + "OrdersController.java.OrdersController.OK---->" + 88);
         return "all_shop_orders";
